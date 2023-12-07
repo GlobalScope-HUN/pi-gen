227024695b78ec2a42e0a371636beead7e4810d5
Attila Gombos <attilagombos@syntaxsugar.net>
2023-12-07 14:59:58 +0100

    Add setup for inventory plugin path

--- stage3/02-add-ansible-resources/01-run.sh
+++ stage3/02-add-ansible-resources/01-run.sh
@@ -1,2 +1,9 @@
 
 cp -r "${BASE_DIR}/../EffectiveRange-devc/ansible" "${ROOTFS_DIR}/etc/ansible"
+
+on_chroot << EOF
+
+# Set up Ansible inventory plugins path
+export ANSIBLE_INVENTORY_PLUGINS=/etc/ansible/plugins
+
+EOF
