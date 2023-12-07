2cbf71c1396b0d421e8dd59bf2055aff0499de6f
Attila Gombos <attilagombos@syntaxsugar.net>
2023-12-07 19:52:58 +0100

    Install ssdpy

--- stage3/02-add-ansible-resources/01-run.sh
+++ stage3/02-add-ansible-resources/01-run.sh
@@ -3,6 +3,9 @@ cp -r "${BASE_DIR}/../EffectiveRange-devc/ansible" "${ROOTFS_DIR}/etc/ansible"
 
 on_chroot << EOF
 
+# Install ssdpy Python module
+pip3 install ssdpy
+
 # Set up Ansible inventory plugins path
 export ANSIBLE_INVENTORY_PLUGINS=/etc/ansible/plugins
 
