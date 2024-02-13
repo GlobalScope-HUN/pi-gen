b414077276045680607a119202fb80f21f271da8
attilagombos <attilagombos@syntaxsugar.net>
2024-02-13 14:56:01 +0000

    Update wifi-manager installation

--- stage3/01-install-wifi-manager/01-run.sh
+++ stage3/01-install-wifi-manager/01-run.sh
@@ -1,15 +1,17 @@
 
-cp -r "${BASE_DIR}/../EffectiveRange/poc/wifiManager" "${ROOTFS_DIR}/tmp/wifiManager"
+mkdir -p "${ROOTFS_DIR}/etc/wifi-manager"
+
+cp -r "${BASE_DIR}/../EffectiveRange/poc/wifiManager" "${ROOTFS_DIR}/etc/wifi-manager/wifiManager"
 
 on_chroot << EOF
 
-cd /tmp/wifiManager
+cd /etc/wifi-manager/wifiManager
 
 # Package wifi-manager
 dpkg-buildpackage -us -uc -b
 
 # Install wifi-manager
-apt-get install /tmp/wifi-manager_0.0.1_all.deb
+apt-get install /etc/wifi-manager/wifi-manager_*_all.deb
 
 # Cleanup
 apt-get remove -y dh-virtualenv devscripts
